b5d5f237b5106c1e017ac970233455f877946e34
bareknuckle <thomas-wernicke@gmx.de>
2015-05-24 22:45:16 +0200

    New MainActivity, TODO SQLite

---
+++ app/src/main/java/com/example/ise/mis/DBAdapter.java
@@ -0,0 +1,145 @@
+package com.example.ise.mis;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBAdapter {
+
+    private static final String TAG = "DBAdapter"; //used for logging database version changes
+
+    // Field Names:
+    public static final String KEY_ROWID = "id";
+    public static final String KEY_SUBJECT = "subject";
+    public static final String KEY_NOTICE = "notice";
+
+    public static final String[] ALL_KEYS = new String[] {KEY_ROWID, KEY_SUBJECT, KEY_NOTICE};
+
+    // Column Numbers for each Field Name:
+    public static final int COL_ROWID = 0;
+    public static final int COL_TASK = 1;
+    public static final int COL_DATE = 2;
+
+    // DataBase info:
+    public static final String DATABASE_NAME = "dbNotice";
+    public static final String DATABASE_TABLE = "mainNotice";
+    public static final int DATABASE_VERSION = 2; // The version number must be incremented each time a change to DB structure occurs.
+
+    //SQL statement to create database
+    private static final String DATABASE_CREATE_SQL =
+            "CREATE TABLE " + DATABASE_TABLE
+                    + " (" + KEY_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + KEY_SUBJECT + " TEXT NOT NULL, "
+                    + KEY_NOTICE + " TEXT"
+                    + ");";
+
+    private final Context context;
+    private DatabaseHelper myDBHelper;
+    private SQLiteDatabase db;
+
+
+    public DBAdapter(Context ctx) {
+        this.context = ctx;
+        myDBHelper = new DatabaseHelper(context);
+    }
+
+    // Open the database connection.
+    public DBAdapter open() {
+        db = myDBHelper.getWritableDatabase();
+        return this;
+    }
+
+    // Close the database connection.
+    public void close() {
+        myDBHelper.close();
+    }
+
+    // Add a new set of values to be inserted into the database.
+    public long insertRow(String subject, String notice) {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_SUBJECT, subject);
+        initialValues.put(KEY_NOTICE, notice);
+
+        // Insert the data into the database.
+        return db.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    // Delete a row from the database, by rowId (primary key)
+    public boolean deleteRow(long rowId) {
+        String where = KEY_ROWID + "=" + rowId;
+        return db.delete(DATABASE_TABLE, where, null) != 0;
+    }
+
+    public void deleteAll() {
+        Cursor c = getAllRows();
+        long rowId = c.getColumnIndexOrThrow(KEY_ROWID);
+        if (c.moveToFirst()) {
+            do {
+                deleteRow(c.getLong((int) rowId));
+            } while (c.moveToNext());
+        }
+        c.close();
+    }
+
+    // Return all data in the database.
+    public Cursor getAllRows() {
+        String where = null;
+        Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS, where, null, null, null, null, null);
+        if (c != null) {
+            c.moveToFirst();
+        }
+        return c;
+    }
+
+    // Get a specific row (by rowId)
+    public Cursor getRow(long rowId) {
+        String where = KEY_ROWID + "=" + rowId;
+        Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS,
+                where, null, null, null, null, null);
+        if (c != null) {
+            c.moveToFirst();
+        }
+        return c;
+    }
+
+    // Change an existing row to be equal to new data.
+    public boolean updateRow(long rowId, String subject, String notice) {
+        String where = KEY_ROWID + "=" + rowId;
+        ContentValues newValues = new ContentValues();
+        newValues.put(KEY_SUBJECT, subject);
+        newValues.put(KEY_NOTICE, notice);
+        // Insert it into the database.
+        return db.update(DATABASE_TABLE, newValues, where, null) != 0;
+    }
+
+
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase _db) {
+            _db.execSQL(DATABASE_CREATE_SQL);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase _db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading application's database from version " + oldVersion
+                    + " to " + newVersion + ", which will destroy all old data!");
+
+            // Destroy old database:
+            _db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+
+            // Recreate new database:
+            onCreate(_db);
+        }
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/ise/mis/createNotice.java
@@ -0,0 +1,104 @@
+package com.example.ise.mis;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.example.ise.mis.DBAdapter;
+
+/**
+ * Created by twernicke on 5/24/2015.
+ */
+public class createNotice extends ActionBarActivity {
+
+    DBAdapter noticeDB;
+
+    private EditText mEditTextNoticeSubject;
+    private EditText mEditTextNotice;
+    private Button mButtonEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_notice);
+
+        mEditTextNoticeSubject = (EditText)findViewById(R.id.editText_noticeSubject);
+        mEditTextNotice = (EditText)findViewById(R.id.edNotice);
+        mButtonEmail = (Button)findViewById(R.id.button_main_eMail);
+
+        //TODO DB
+        //openDB();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+//TODO DB
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        closeDB();
+//    }
+//
+//    private void openDB() {
+//        noticeDB = new DBAdapter(this);
+//        noticeDB.open();
+//    }
+//
+//    private void closeDB() {
+//        noticeDB.close();
+//    }
+
+
+    public void onClickSaveNotice(View view) {
+
+        //TODO DB
+        //noticeDB.insertRow(mEditTextNoticeSubject.getText().toString(),
+        //                    mEditTextNotice.getText().toString());
+
+        Intent intent = new Intent(this, com.example.ise.mis.MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onMapButtonClick(View view) {
+        Intent intent = new Intent(this, map.class);
+        startActivity(intent);
+    }
+
+    public void onClickEmail(View view) {
+        final Intent intent = new Intent(this, com.example.ise.mis.sendHighlightedNotice.class);
+        mButtonEmail.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                intent.putExtra("subject", mEditTextNoticeSubject.getText().toString());
+                                                intent.putExtra("notice", mEditTextNotice.getText().toString());
+                                                startActivity(intent);
+                                            }
+                                        }
+        );
+    }
+}
